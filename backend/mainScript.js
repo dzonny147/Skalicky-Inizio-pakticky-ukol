0fef556042f983524df4fbc0ecea748c26e76819
dzonny147 <79198241+dzonny147@users.noreply.github.com>
2024-12-02 16:57:50 +0100

    Update mainScript.js

--- backend/mainScript.js
+++ backend/mainScript.js
@@ -32,4 +32,4 @@ app.post('/scrape', async (req, res) => {
 app.use('/output', express.static(path.join(__dirname, '../output')));
 
 // Start serveru
-app.listen(PORT, () => console.log(`Server running on https://dzonny147-github-io.onrender.com:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on http://localhost:10000:${PORT}`));
