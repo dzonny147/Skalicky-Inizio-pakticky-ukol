78c003826b0dc7ff324c921e4c12f0276b76786d
dzonny147 <79198241+dzonny147@users.noreply.github.com>
2024-12-02 15:47:56 +0100

    Update mainScript.js

--- backend/mainScript.js
+++ backend/mainScript.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const { scrapeGoogle } = require('./scraper');
-const { saveResultsToFile } = require('./utils/saveToFile');
+const { saveResultsToFile } = require('./utils/saveToFile.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
